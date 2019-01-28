08cc4cd93f9eeaddccc383dbfe1ab8eeb0d34e9a
NghiemDat <nghiemdat9889@gmail.com>
2019-01-28 11:18:54 +0700

    Chapter 9:Advanced login

--- Gemfile
+++ Gemfile
@@ -14,9 +14,9 @@ gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.2"
 gem "rails-i18n"
 gem "sass-rails", "~> 5.0"
-gem "sqlite3"
-gem "turbolinks"
 gem "uglifier", ">= 1.3.0"
+gem "turbolinks"
+gem "sqlite3"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,47 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  attr_reader :remember_token
+  
   validates :name, presence: true,
             length: {maximum: Settings.user_valid.max_length_name}
   validates :email, presence: true,
             length: {maximum: Settings.user_valid.max_length_email},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
-  validates :password, presence: true,
+  validates :password, presence: true, allow_nil: true,
             length: {minimum: Settings.user_valid.min_length_password}
   before_save :email_downcase
   has_secure_password
+  
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update remember_digest: User.digest(@remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   private
 
   def email_downcase
     email.downcase!
-  end
-
-  def self.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create string, cost: cost
   end
 end
